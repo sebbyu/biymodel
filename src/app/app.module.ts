1651f2f786a10d7a1524ef6e33d78aa3f7f69ee2
sebbyu <you.sebastian.1.618@gmail.com>
2021-02-27 23:17:46 -0800

    three pages uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { Page404Component } from './page404/page404.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    Page404Component,
+    ComingsoonComponent
   ],
   imports: [
     BrowserModule,

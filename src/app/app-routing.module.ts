3983e84142b9a66b2b701520cdc2f543bc77f4cb
sebbyu <you.sebastian.1.618@gmail.com>
2021-02-27 23:57:42 -0800

    Added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { Page404Component } from './page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'comingsoon', component: ComingsoonComponent },
+  { path: 'pagenotfound', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
